8fc68f5b743396273caedcfb9fb97a6769f7e529
ums1212 <voll1212@gmail.com>
2024-11-24 00:44:56 +0900

    [Feat] 프로젝트 기간 추가

--- lib/common/portfolio_list.dart
+++ lib/common/portfolio_list.dart
@@ -6,7 +6,7 @@ List<Portfolio> androidList = [
       "assets/images/unipiece.png",
       "유니피스",
       "https://github.com/APP-Android2/FinalProject-ShoppingMallService-team6",
-      "대학 졸업 작품 매입 판매 및 전시 쇼핑몰 앱\n\n마이페이지, 회원정보 수정, 작가 정보, 전시실 신청 기능 담당",
+      "2024-03 ~ 2024-04\n\n대학 졸업 작품 매입 판매 및 전시 쇼핑몰 앱\n\n마이페이지, 회원정보 수정, 작가 정보, 전시실 신청 기능 담당",
       ["Firebase", "Firestore", "Glide", "LiveData", "DataBinding", "SNS Login", "MVVM"],
       "./assets/pdf/unipiece.pdf"
   ),
@@ -14,7 +14,7 @@ List<Portfolio> androidList = [
       "assets/images/modigm.png",
       "모우다임",
       "https://github.com/APP-Android2/FinalProject-modigm",
-      "개발자 스터디 모집 앱\n\n회원가입 기능 담당",
+      "2024-05 ~ 2024-11\n\n개발자 스터디 모집 앱\n\n회원가입 기능 담당",
       ["Firebase", "Firestore", "FirebaseAuthentication", "Glide", "ViewBinding", "DataBinding", "StateFlow", "SNS Login", "MVVM"],
     null
   ),
@@ -22,7 +22,7 @@ List<Portfolio> androidList = [
       "assets/images/moviefriends.png",
       "무비프렌즈",
       "https://github.com/ums1212/movie-friends",
-      "영화 친구 만들기 커뮤니티 앱\n\n개인프로젝트, TMDB 영화 정보 API, LBS, 채팅",
+      "2024-10 ~ 2024-11\n\n영화 친구 만들기 커뮤니티 앱\n\n개인프로젝트, TMDB 영화 정보 API, LBS, 채팅",
       ["Jetpack Compose","Firebase","Coil","Hilt","Flow","StateFlow","MVVM","OAuth2", "Fused Location", "REST API", "Kakao Local API", "okHttp3", "Retrofit2"],
     null
   ),
@@ -33,7 +33,7 @@ List<Portfolio> flutterList = [
     "assets/images/myworldcup.png",
     "내가 만든 월드컵",
     "https://github.com/valborgs/my-worldcup-local",
-    "이상형 월드컵 게임 앱",
+    "2024-06\n\n이상형 월드컵 게임 앱",
     ["Flutter","Dart","Provider","sqlite3","kakao message"],
     null
   ),
